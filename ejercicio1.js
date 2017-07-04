01dd8792ea203519119eea4ac62285e867fbceb3
yudithchuctaya <yudithyesicachuctaya@gmail.com>
2017-07-04 23:39:57 +0200

    mi practica

---
+++ ejercicio1.js
@@ -0,0 +1,23 @@
+var min = ;
+var max= ;
+/*var respuesta;
+var numero = parseInt(prompt("ingrese un numero"));
+
+
+/*if (respuesta ==1){
+  maximo = numero ;
+  minimo = numero ;
+}
+if (respuesta > 1 && numero > maximo ){
+  numero = maximo;
+}
+if (respuesta > 1 && numero < minimo){
+  numero = minimo;
+}*/
+var n1 = 10 ;
+var n2 = 30;
+var n3 = 5 ;
+math.maximo(n1 ,n2 ,n3);
+math.minimo(n1 ,n2 ,n3);
+console.log(min);
+console .log (max);
---
+++ ejercicio2.js
@@ -0,0 +1,12 @@
+var a = [15 ,29,78,29,40 , 20 ,18 ,100,78 , 68];
+var par = "";
+var impar = "";
+for (var i = 0 ;i < a.length; i++;){
+  if (a % 2 ==0 ) {
+    return par;
+
+  } else {
+    return impar;
+  }
+
+}console.log (a);
